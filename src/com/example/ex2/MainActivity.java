5afbfc04c256896fb638170559e155958810343d
ITLAB <ITLAB@IT206>
2017-01-31 14:19:18 +0530

    4th commit

--- src/com/example/ex2/MainActivity.java
+++ src/com/example/ex2/MainActivity.java
@@ -1,35 +1,88 @@
 package com.example.ex2;
-
-import android.app.Activity;
+ 
+import android.app.AliasActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends Activity {
-
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+ 
+public class MainActivity extends AliasActivity implements OnClickListener
+{
+    //Defining the Views
+    EditText Num1;
+    EditText Num2;
+    Button Add;
+    Button Sub;
+    Button Mul;
+    Button Div;
+    TextView Result;
+ 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+ 
+        //Referring the Views
+        Num1 = (EditText) findViewById(R.id.n1);
+        Num2 = (EditText) findViewById(R.id.n2);
+        Add = (Button) findViewById(R.id.plus);
+        Sub = (Button) findViewById(R.id.subtract);
+        Mul = (Button) findViewById(R.id.multiply);
+        Div = (Button) findViewById(R.id.divide);
+        Result = (TextView) findViewById(R.id.result);
+ 
+        // set a listener
+        Add.setOnClickListener(this);
+        Sub.setOnClickListener(this);
+        Mul.setOnClickListener(this);
+        Div.setOnClickListener(this);
     }
-
+     
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick (View v)
+    {
+         
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+        String oper = "";
+ 
+        // check if the fields are empty
+        if (TextUtils.isEmpty(Num1.getText().toString()) || TextUtils.isEmpty(Num2.getText().toString()))
+                return;
+ 
+        // read EditText and fill variables with numbers
+        num1 = Float.parseFloat(Num1.getText().toString());
+        num2 = Float.parseFloat(Num2.getText().toString());
+ 
+        // defines the button that has been clicked and performs the corresponding operation
+        // write operation into oper, we will use it later for output
+        switch (v.getId()) 
+        {
+            case R.id.plus:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.subtract:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.multiply:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.divide:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
         }
-        return super.onOptionsItemSelected(item);
+        // form the output line
+        Result.setText(num1 + " " + oper + " " + num2 + " = " + result);
     }
 }
